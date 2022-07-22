fec89137f31784743ab9f93ed21e07a20924b9b0
atalhaablak <109148636+bastechinavation@users.noreply.github.com>
2022-07-22 22:49:45 +0300

    Created FoodLife Home Page

---
+++ login_app/lib/screens/food_app/food_home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FoodHomePage extends StatefulWidget {
+  const FoodHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<FoodHomePage> createState() => _FoodHomePageState();
+}
+
+class _FoodHomePageState extends State<FoodHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
